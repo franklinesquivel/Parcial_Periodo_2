3f9047d3c1579c12506484e1040d2d59c169be21
Frank <franklin.esquivel@outlook.com>
2018-04-06 08:17:07 -0600

    Controladores y recursos

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('cuentas', 'CuentaController');
+Route::resource('usuarios', 'UserController');
